cfd11bb1e63dc6de016a4bd942a78d0ff87cc614
Toshi7878 <toshiyuki19991125@gmail.com>
2025-11-23 01:00:45 +0900

    update

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 export { buildTypingMap } from "./build-typing-map/build-map";
 export { generateTypingWord } from "./build-typing-map/generate-typing-word";
 export { sentenceToKanaChunkWords } from "./build-typing-map/sentence-to-kana-chunk-words";
+export { isTypingKey } from "./evaluator/is-typing-key";
 export type { TypingEvaluationResult, TypingKey } from "./evaluator/type";
 export {
   evaluateKanaTypingInput,

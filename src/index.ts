81d02ea0c724f6aa6e613d2b76c9fd7a2db34bdb
Toshi7878 <toshiyuki19991125@gmail.com>
2026-06-06 17:52:42 +0900

    fix: consolidate exports from evaluator constants to include DAKU_HANDAKU_NORMALIZE_MAP

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 export { buildTypingMap } from "./build-typing-map/build-map.js";
-export { CODE_TO_KANA, KEY_TO_KANA } from "./evaluator/const.js";
 export {
   createDisplayWord,
   replaceAllSpaceWithLowMacron,
@@ -7,6 +6,7 @@ export {
 } from "./create-typing-word/create-display-word.js";
 export { createTypingWord } from "./create-typing-word/create-typing-word.js";
 export { recreateTypingWord } from "./create-typing-word/recreate-typing-word.js";
+export { CODE_TO_KANA, DAKU_HANDAKU_NORMALIZE_MAP, KEY_TO_KANA } from "./evaluator/const.js";
 export { handleTyping } from "./evaluator/handle-typing.js";
 export { isTypingKey } from "./evaluator/is-typing-key.js";
 export type { TypingInputResult } from "./evaluator/type.js";
